ab575d4477c000f71126990afe93a93efb7f810a
Kairgeldin Dmitry <dmitriy.k@densertech.com>
2025-09-28 20:48:15 +0500

    rm: nav-extra

--- src/widgets/navigation/index.ts
+++ src/widgets/navigation/index.ts
@@ -6,5 +6,4 @@
 export { AppSidebar } from "./app-sidebar";
 export { ConditionalSidebarLayout } from "./conditional-sidebar-layout";
 export { NavMain } from "./nav-main";
-export { NavExtra } from "./nav-extra";
 export { NavUser } from "./nav-user";
