82936e656c0b35e4dd3abd088b584b938f915460
Kairgeldin Dmitry <dmitriy.k@densertech.com>
2026-01-19 21:14:11 +0500

    linter

--- src/widgets/calendar-widget/month-view/month-event-badge.tsx
+++ src/widgets/calendar-widget/month-view/month-event-badge.tsx
@@ -48,8 +48,7 @@ const eventBadgeVariants = cva(
       multiDayPosition: {
         first:
           "relative z-10 mr-0 w-[calc(100%-3px)] rounded-r-none border-r-0 [&>span]:mr-2.5",
-        middle:
-          "relative z-10 mx-0 w-[calc(100%+1px)] rounded-none border-x-0",
+        middle: "relative z-10 mx-0 w-[calc(100%+1px)] rounded-none border-x-0",
         last: "ml-0 rounded-l-none border-l-0",
         none: "",
       },
