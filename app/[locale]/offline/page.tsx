20ebed18047ce21cc4e13b8971b3a30377f567c0
Kairgeldin Dmitry <dmitriy.k@densertech.com>
2026-02-06 17:51:04 +0500

    upd: refresh to main page

--- app/[locale]/offline/page.tsx
+++ app/[locale]/offline/page.tsx
@@ -2,9 +2,11 @@
 
 import { Button } from "@/src/entities";
 import { useTranslations } from "next-intl";
+import { useRouter } from "next/navigation";
 
 export default function OfflinePage() {
   const t = useTranslations("Offline");
+  const router = useRouter();
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4">
@@ -12,7 +14,7 @@ export default function OfflinePage() {
         <div className="text-6xl">📡</div>
         <h1 className="text-3xl font-bold">{t("title")}</h1>
         <p className="text-gray-200 max-w-md">{t("description")}</p>
-        <Button onClick={() => window.location.reload()}>{t("retry")}</Button>
+        <Button onClick={() => router.push("/")}>{t("retry")}</Button>
       </div>
     </div>
   );
